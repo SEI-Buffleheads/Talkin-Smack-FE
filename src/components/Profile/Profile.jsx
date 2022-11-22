da5cc445f7cc9a715cca1294cb2352a57432d5b5
Lin Lin <linlin@Lins-MacBook-Air.local>
2022-11-22 12:17:34 -0500

    profile base css layout

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,35 @@ import './Profile.css';
 function Profile() {
   return (
     <>
-      <h1>Hello Profile</h1>
+      <div className='profile-container'>
+        <div className='user-avatar'>
+          <div className='avatar-img'>😱</div>
+          <button>Edit</button>
+        </div>
+        <div className='profile-info'>
+        <input
+          type="text"
+          // value={username}
+          placeholder="Username"
+          name="username"
+          id="username"
+          // onChange={(e) => setUsername(e.target.value)}
+          />
+        <input 
+            type="text"
+            placeholder='Password'
+            name='password'
+            id='password'
+          />
+          <input
+            type="text"
+            placeholder='Verify Password'
+            name='confirm-password'
+            id='confirm-password'
+          />
+        </div>
+        <button>Update</button>
+      </div>
     </>
   );
 }
