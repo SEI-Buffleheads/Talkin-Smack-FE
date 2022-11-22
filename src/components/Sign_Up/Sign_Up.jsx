93e9472bdaeac177223bbe081a626949c84e4a1f
chelseadgls <chelseamdouglas@gmail.com>
2022-11-22 17:02:47 -0500

    updates to signup form

--- src/components/Sign_Up/Sign_Up.jsx
+++ src/components/Sign_Up/Sign_Up.jsx
@@ -3,6 +3,8 @@ import { useState, useNavigate } from 'react';
 
 function Sign_Up() {
 
+  const [firstname, setFirstName] = useState('')
+  const [lastname, setLastName] = useState('')
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('');
@@ -10,18 +12,25 @@ function Sign_Up() {
   const [valid, setValid] = useState('');
 
   const handleChange = e => {
-    if (e.target.id === 'username') {
+    if (e.target.id === 'first_name') {
+      setFirstName(e.target.value)
+    } else if (e.target.id === "last_name"){
+      setLastName(e.target.value)
+    } else if (e.target.id === "username") {
       setUsername(e.target.value)
-    } else if (e.target.id === 'password') {
+    } else if (e.target.id === "email") {
+      setEmail(e.target.value)
+    }
+    else if (e.target.id === 'password') {
       setPassword(e.target.value)
-    } else if (e.target.id === 'passwordConfirm') {
+    } else if (e.target.id === 'confirm_password') {
       setPasswordConfirm(e.target.value)
     }
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    e.target[1].value === e.target[2].value ? setValid("Password correct.") : setValid("Try your password again.")
+    // e.target[1].value === e.target[2].value ? setValid("Password correct.") : setValid("Try your password again.")
   }
 
   return (
@@ -29,11 +38,14 @@ function Sign_Up() {
       <div className="form">
         <h1>Sign Up</h1>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Username" onChange={handleChange} />
-          <input type="password" placeholder="Password" onChange={handleChange} />
-          <input type="password" placeholder="Confirm Password" onChange={handleChange} />
+          <input type="text" placeholder="First Name" onChange={handleChange} id="first_name" />
+          <input type="text" placeholder="Last Name" onChange={handleChange} id="last_name" />
+          <input type="text" placeholder="Email" onChange={handleChange} id="email" />
+          <input type="text" placeholder="Username" onChange={handleChange} id="username" />
+          <input type="password" placeholder="Password" onChange={handleChange} id="password" />
+          <input type="password" placeholder="Confirm Password" onChange={handleChange} id="confirm_password" />
           <input type="submit" value="Submit" />
-          <h1>{valid}</h1>
+          {/* <h1>{valid}</h1> */}
         </form>
       </div>
     </div>
