dbe7489eb8e1f319200f785dc92c38626922ef94
CapriceLoren <capriceloren@gmail.com>
2022-11-22 13:58:45 -0500

    update footer

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import "./Navbar.css";
 import { Link, Routes, Route } from "react-router-dom";
-<script src="https://kit.fontawesome.com/e3e9f2d35b.js" crossorigin="anonymous"></script>
 
 
 
@@ -8,13 +7,6 @@ function Navbar() {
   return (
     <>
       <h1>Hello Navbar</h1>
-      <nav>
-        <div className="nav-container">
-        <i class="fa-regular fa-circle-user"></i>
-          <div className="nav-img">🤬</div>
-          <Link to="/profile">Profile </Link>
-        </div>
-      </nav>
     </>
   );
 }
