3adad0fab590715f6bffa007c6364debbfc9b55a
Malik Washington <malikwashington@gmail.com>
2022-11-27 03:28:10 -0500

    installed csstransistion from react-trasistion group, made the modal and now it animates onto the screen while attaching itself to the dom and removes itself from the dom

--- src/components/Talk_Smack/Talk_Smack.jsx
+++ src/components/Talk_Smack/Talk_Smack.jsx
@@ -1,10 +1,24 @@
-import './Talk_Smack.css';
+import './Talk_Smack.css'
+import { CSSTransition } from "react-transition-group";
 
-function Talk_Smack() {
+
+function Talk_Smack({ show, close }) {
+
   return (
-    <>
-      <h1>Hello Talk_Smack</h1>
-    </>
+    <CSSTransition in={show} unmountOnExit timeout={{ enter: 0, exit: 300 }}>
+
+    <div className={`modal ${show ? 'show' : ''}`}>
+      <div className="modal-content">
+        <div className="modal-header">
+          <h4 className="modal-title">Talk Smack!</h4>
+        </div>
+        <div className="modal-body">This is the Smack Talk</div>
+        <div className="modal-footer">
+          <button onClick={close}>close</button>
+        </div>
+      </div>
+    </div>
+    </CSSTransition>
   );
 }
 
