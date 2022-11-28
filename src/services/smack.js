bcde45af5131aa1b6d2e787a696d751db8711339
Malik Washington <malikwashington@gmail.com>
2022-11-28 11:14:35 -0500

    added services, apiConfig, and the beginning of api calls for posting commenting etc.

---
+++ src/services/smack.js
@@ -0,0 +1,46 @@
+import api from "./apiConfig.js";
+
+export const getAlbums = async () => {
+  try {
+    const response = await api.get("/albums");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getAlbum = async (id) => {
+  try {
+    const response = await api.get(`/albums/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createAlbum = async (albumData) => {
+  try {
+    const response = await api.post("/albums", albumData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateAlbum = async (id, albumData) => {
+  try {
+    const response = await api.put(`/albums/${id}`, albumData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteAlbum = async (id) => {
+  try {
+    const response = await api.delete(`/albums/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  } // remove from the artist as well in backend
+};
